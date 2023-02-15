eb03d543254a6cda70bec79863f12728576fe318
Alexander Suslin <asuslin01@gmail.com>
2023-02-15 15:50:51 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+import './slider';
+
+import changeModalState from './modules/changeModalState';
+import forms from './modules/forms';
+import modals from './modules/modals';
+import tabs from './modules/tabs';
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    let modalState = {};
+
+    changeModalState(modalState);
+    modals();
+    tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+    tabs(
+        '.decoration_slider',
+        '.no_click',
+        '.decoration_content > div > div',
+        'after_click'
+    );
+    tabs(
+        '.balcon_icons',
+        '.balcon_icons_img',
+        '.big_img > img',
+        'do_image_more',
+        'inline_block'
+    );
+    forms();
+});
+
